aa8cf24c84ac63c5a5c8ccb6189742fae0fb3805
Xab3r <mail.xaber@gmail.com>
2023-02-10 00:50:10 +0100

    Added interface to MetadataReferenceCache

---
+++ src/CSScriptLib/src/CSScriptLib/IMetadataReferencesCache.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using Microsoft.CodeAnalysis;
+
+namespace CSScriptLib
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IMetadataReferencesCache
+    {
+        /// <summary>
+        /// Gets metadata by Assembly
+        /// </summary>
+        /// <param name="assembly"></param>
+        /// <returns></returns>
+        MetadataReference Get(Assembly assembly);
+
+        /// <summary>
+        /// Gets metadata for all assemblies in current appdomain
+        /// </summary>
+        /// <returns></returns>
+        IReadOnlyList<MetadataReference> GetDomainReferences();
+    }
+}
